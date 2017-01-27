7ae86438c93e575d140cc9a713a9e1f50cbf0209
chanakyavikas <chanakyavikas@gmail.com>
2017-01-27 09:06:35 +0000

    cloudFirst commit

---
+++ src/main/java/com/example/CloudFoundryFirstApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFoundryFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFoundryFirstApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/SimpleHello.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 General Electric Company. All rights reserved.
+ *
+ * The copyright to the computer software herein is the property of
+ * General Electric Company. The software may be used and/or copied only
+ * with the written permission of General Electric Company or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the software has been supplied.
+ */
+ 
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author predix -
+ */
+@RestController
+public class SimpleHello {
+
+	@RequestMapping("/")
+	public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
